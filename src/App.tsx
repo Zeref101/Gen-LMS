1bbb6d6cfc3c3688fd7951679bde833d2fbec0c9
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-15 02:44:24 +0530

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import student from "../public/images/man-with-laptop-studying-or-working-concept-removebg-preview.png"
-import GlobalSearch from "./components/search/GlobalSearch"
-
-
-function App() {
+const App = () => {
 
   return (
-    <main className='h-screen bg-white'>
-      <section className="flex flex-col gap-8 justify-center items-center">
-        <GlobalSearch />
-      </section>
-
-      <img src={student} alt="student" className="w-[]" />
-    </main>
+    <>
+    </>
   )
 }
-
 
 export default App
