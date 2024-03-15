00cd7f980dbdd9749a8503ac8540c6974a46eb41
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 01:19:00 +0530

    Add user authentication functions

---
+++ src/lib/backend/User.ts
@@ -0,0 +1,46 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { app } from "./firebase.ts"; // import the app from your firebase.ts file
+// import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+// ...
+
+// const auth = getAuth(app);
+
+export const handleSignup = async (email: string, password: string) => {
+  const auth = getAuth(app);
+  //   const db = getFirestore(app);
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+
+    // Store additional user data in Firestore
+    // await setDoc(doc(db, "userftfs", user.uid), {
+    //   username,
+    //   role,
+    // });
+
+    return user;
+  } catch (error) {
+    // Handle error
+  }
+};
+
+export const handleSignIn = async (email: string, password: string) => {
+  const auth = getAuth(app);
+  try {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    return user;
+    // ...
+  } catch (error) {
+    // Handle error
+  }
+};
