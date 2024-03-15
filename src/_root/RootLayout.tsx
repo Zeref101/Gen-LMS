51ef1a733d9edca625c4f6c77a7f38ad827456c4
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 04:02:27 +0530

    Refactor RootLayout component

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,26 +1,14 @@
 // import LeftSidebar from '@/components/LeftSidebar';
 import { AuthContextWrap } from '@/context/AuthContext';
-import { useContext, useEffect } from 'react';
-import { Navigate, Outlet, useNavigate } from 'react-router-dom';
+import { useContext, } from 'react';
+import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
     const { isAuthenticated } = useContext(AuthContextWrap);
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        if (!isAuthenticated) {
-            navigate('/sign-up');
-        } else {
-            navigate('/');
-
-        }
-    }, [isAuthenticated, navigate]);
 
     return (
         <>
-            {!isAuthenticated ? (
-                <Navigate to="/sign-up" />
-            ) : (
+            {isAuthenticated && (
                 <>
                     <section className='flex flex-1 justify-center items-center flex-col'>
                         {/* Outlet will render the child route that matches the current URL */}
@@ -28,6 +16,8 @@ const RootLayout = () => {
                     </section>
                 </>
             )}
+
+
 
         </>
     );
