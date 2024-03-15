3fbb63e1f8b8c2444c8195ea3847acd18c6eab3c
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 02:37:38 +0530

    Refactor handleSignup function in User.ts

--- src/lib/backend/User.ts
+++ src/lib/backend/User.ts
@@ -4,15 +4,9 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 import { app } from "./firebase.ts"; // import the app from your firebase.ts file
-// import { getFirestore, doc, setDoc } from "firebase/firestore";
-
-// ...
-
-// const auth = getAuth(app);
 
 export const handleSignup = async (email: string, password: string) => {
   const auth = getAuth(app);
-  //   const db = getFirestore(app);
   try {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
@@ -20,12 +14,6 @@ export const handleSignup = async (email: string, password: string) => {
       password
     );
     const user = userCredential.user;
-
-    // Store additional user data in Firestore
-    // await setDoc(doc(db, "userftfs", user.uid), {
-    //   username,
-    //   role,
-    // });
 
     return user;
   } catch (error) {
