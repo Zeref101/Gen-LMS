6e986ff538be882c07e495c7bcfb62d89f4936be
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 21:29:03 +0530

    Refactor RootLayout component to include LeftSidebar

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,4 +1,5 @@
 // import LeftSidebar from '@/components/LeftSidebar';
+import LeftSidebar from '@/components/LeftSidebar';
 import { AuthContextWrap } from '@/context/AuthContext';
 import { useContext, } from 'react';
 import { Outlet } from 'react-router-dom';
@@ -10,10 +11,16 @@ const RootLayout = () => {
         <>
             {isAuthenticated && (
                 <>
-                    <section className='flex flex-1 justify-center items-center flex-col'>
-                        {/* Outlet will render the child route that matches the current URL */}
-                        <Outlet />
-                    </section>
+                    <div className='w-full h-full p-6 rounded-lg drop-shadow-lg'>
+                        <div className='flex w-full h-full bg-white rounded-lg drop-shadow-lg p-4'>
+                            <div className=' w-[400px]'>
+                                <LeftSidebar />
+                            </div>
+                            <section className='flex flex-1 justify-center items-center flex-col'>
+                                <Outlet />
+                            </section>
+                        </div>
+                    </div>
                 </>
             )}
 
