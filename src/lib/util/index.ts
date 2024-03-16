22f7e4ccb8b61b348b14f4662fea8adc22de2c8c
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-17 01:59:32 +0530

    Add timestampToDate function to convert timestamp to Date object

---
+++ src/lib/util/index.ts
@@ -0,0 +1,5 @@
+export function timestampToDate({ seconds, nanoseconds }) {
+  // Convert seconds to milliseconds and add nanoseconds converted to milliseconds
+  const milliseconds = seconds * 1000 + nanoseconds / 1000000;
+  return new Date(milliseconds);
+}
