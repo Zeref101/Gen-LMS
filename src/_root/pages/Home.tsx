66c3108552c1f2680d4b802e548cc9b7ee617331
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 22:54:31 +0530

    Refactor code for improved performance and readability

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -4,9 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { fetchUserCourses } from "@/lib/backend/User";
 import { DocumentData } from 'firebase/firestore';
 import { useEffect, useState } from "react";
+import { InfinitySpin } from 'react-loader-spinner';
 
 export default function Home() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
   // const [courses, setCourses] = useState<DocumentData[]>([]);
   const [userCourses, setUserCourses] = useState<DocumentData>([]);
 
@@ -21,6 +24,8 @@ export default function Home() {
         }
       } catch (error) {
         console.error('Error fetching user courses:', error);
+      } finally {
+        setLoading(false);
       }
     };
     fetchCourses();
@@ -171,119 +176,130 @@ export default function Home() {
   const currentDate = new Date().toJSON().slice(0, 10);
   console.log(currentDate);
   return (
-    <div className=" p-4 h-screen w-full">
-      <div className="bg-white flex justify-center items-center flex-col rounded-3xl h-full overflow-hidden p-6">
-        <div className="flex self-start justify-between items-center">
-          <div className="flex space-x-4 items-center">
-            <div className="bg-blue-500 text-white p-4 rounded-full">
-              <i className="fas fa-book"></i>
-            </div>
-            <div>
-              <h1 className="text-2xl font-semibold leading-[140%] text-purple-700 ">Course Activity</h1>
-              <p className="text-gray-400">
-                {new Date().toJSON().slice(0, 10)}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="flex gap-4 h-full w-full">
-          <div className="basis-3/5 h-full bg-gray-50 p-4 rounded-2xl">
-            <div className="flex justify-between items-center">
+    <>
+      {loading ? (
+        <InfinitySpin
+          width="200"
+          color="#7e22ce"
+          ariaLabel="infinity-spin-loading"
+        />
+      ) : (
+        <div className=" p-4 h-screen w-full">
+          <div className="bg-white flex justify-center items-center flex-col rounded-3xl h-full overflow-hidden p-6">
+            <div className="flex self-start justify-between items-center">
               <div className="flex space-x-4 items-center">
-              </div>
-              <div className="flex space-x-4 items-center">
+                <div className="bg-blue-500 text-white p-4 rounded-full">
+                  <i className="fas fa-book"></i>
+                </div>
+                <div>
+                  <h1 className="text-2xl font-semibold leading-[140%] text-purple-700 ">Course Activity</h1>
+                  <p className="text-gray-400">
+                    {new Date().toJSON().slice(0, 10)}
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="h-full overflow-scroll overflow-x-hidden">
-              {userCourses.map((course: DocumentData, index: number) => (
-                <div key={index} className={`flex items-center p-4 rounded-lg`}>
-                  <div
-                    style={{
-                      background: colors[index],
-                    }}
-                    className="w-full h-full rounded-3xl flex items-center space-x-4 p-4 object-contain"
-                  >
-                    <img
-                      className="basis-1/6 w-1/5 h-full"
-                      src={course.image_url}
-                      alt=""
-                    />
-                    <div className="content basis-4/6">
-                      <h3 className="text-white font-semibold">
-                        {course.name}
-                      </h3>
-                      {/* <p className="text-white text-opacity-80 text-sm">
-                        {course.description}
-                      </p> */}
-                    </div>
-                    <button
-                      onClick={() => {
-                        navigate(`/course/${course.course_id}`);
-                      }}
-                      className="justify-center self-end basis-1/6 flex items-center bg-blue-500 h-full p-4 drop-shadow-buttons rounded-3xl text-white"
-                    >
-                      View
-                    </button>
+            <div className="flex gap-4 h-full w-full">
+              <div className="basis-3/5 h-full bg-gray-50 p-4 rounded-2xl">
+                <div className="flex justify-between items-center">
+                  <div className="flex space-x-4 items-center">
+                  </div>
+                  <div className="flex space-x-4 items-center">
                   </div>
                 </div>
-              ))}
-            </div>
-          </div>
-          <div className="flex grow basis-1/5 gap-2 flex-col h-full overflow-hidden">
-            <div className="bg-gray-50 p-4 rounded-2xl h-1/2 basis-1/2">
-              <h2 className=" text-2xl font-semibold text-purple-700">My learning</h2>
-              <div className=" h-full overflow-scroll">
-                {progress.map((course, index) => (
-                  <div
-                    key={index}
-                    className="flex flex-col justify-between items-center"
-                  >
-                    <div className="flex-grow">
-                      <h1 className="font-semibold text-purple-600 text-xl">{course.course}</h1>
-                    </div>
-                    <div className="w-[80%] h-4 rounded-3xl bg-slate-300">
+                <div className="h-full overflow-scroll overflow-x-hidden">
+                  {userCourses.map((course: DocumentData, index: number) => (
+                    <div key={index} className={`flex items-center p-4 rounded-lg`}>
                       <div
                         style={{
-                          width: `${course.progress * 100}%`,
-                          background: `${colors[index % colors.length]}`,
+                          background: colors[index],
                         }}
-                        className={`h-full rounded-3xl`}
-                      ></div>
+                        className="w-full h-full rounded-3xl flex items-center space-x-4 p-4 object-contain"
+                      >
+                        <img
+                          className="basis-1/6 w-1/5 h-full"
+                          src={course.image_url}
+                          alt=""
+                        />
+                        <div className="content basis-4/6">
+                          <h3 className="text-white font-semibold">
+                            {course.name}
+                          </h3>
+                          {/* <p className="text-white text-opacity-80 text-sm">
+                        {course.description}
+                      </p> */}
+                        </div>
+                        <button
+                          onClick={() => {
+                            navigate(`/course/${course.course_id}`);
+                          }}
+                          className="justify-center self-end basis-1/6 flex items-center bg-blue-500 h-full p-4 drop-shadow-buttons rounded-3xl text-white"
+                        >
+                          View
+                        </button>
+                      </div>
                     </div>
-                  </div>
-                ))}
+                  ))}
+                </div>
               </div>
-            </div>
-            <div className="bg-gray-50 p-4 rounded-2xl overflow-hidden basis-1/2">
-              <h2 className="text-2xl font-semibold text-purple-700">Timeline</h2>
-              <div className="flex justify-between items-center mt-4"></div>
-              <div className="mt-4 overflow-scroll h-full">
-                {timelines.map((event, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className="flex justify-center gap-4 items-center"
-                    >
-                      <div className="desc">
-                        <h3>{event.title}</h3>
-                        <p className="text-slate-400 line-clamp-1">{event.description}</p>
+              <div className="flex grow basis-1/5 gap-2 flex-col h-full overflow-hidden">
+                <div className="bg-gray-50 p-4 rounded-2xl h-1/2 basis-1/2">
+                  <h2 className=" text-2xl font-semibold text-purple-700">My learning</h2>
+                  <div className=" h-full overflow-scroll">
+                    {progress.map((course, index) => (
+                      <div
+                        key={index}
+                        className="flex flex-col justify-between items-center"
+                      >
+                        <div className="flex-grow">
+                          <h1 className="font-semibold text-purple-600 text-xl">{course.course}</h1>
+                        </div>
+                        <div className="w-[80%] h-4 rounded-3xl bg-slate-300">
+                          <div
+                            style={{
+                              width: `${course.progress * 100}%`,
+                              background: `${colors[index % colors.length]}`,
+                            }}
+                            className={`h-full rounded-3xl`}
+                          ></div>
+                        </div>
                       </div>
-                      <div className="time">
-                        <p className=" whitespace-nowrap text-slate-600">
-                          {event.date}
-                        </p>
-                        <p className=" whitespace-nowrap text-slate-600">
-                          {event.time}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                })}
+                    ))}
+                  </div>
+                </div>
+                <div className="bg-gray-50 p-4 rounded-2xl overflow-hidden basis-1/2">
+                  <h2 className="text-2xl font-semibold text-purple-700">Timeline</h2>
+                  <div className="flex justify-between items-center mt-4"></div>
+                  <div className="mt-4 overflow-scroll h-full">
+                    {timelines.map((event, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="flex justify-center gap-4 items-center"
+                        >
+                          <div className="desc">
+                            <h3>{event.title}</h3>
+                            <p className="text-slate-400 line-clamp-1">{event.description}</p>
+                          </div>
+                          <div className="time">
+                            <p className=" whitespace-nowrap text-slate-600">
+                              {event.date}
+                            </p>
+                            <p className=" whitespace-nowrap text-slate-600">
+                              {event.time}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
+
   );
 }
