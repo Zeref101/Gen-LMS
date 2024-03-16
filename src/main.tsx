3429f5468eedf6c8df2841dc487d1a5a79e5a056
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-17 01:58:55 +0530

    Add TextSelect component to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { AuthProvider } from './context/AuthContext.tsx'
+import TextSelect from './components/TextSelect.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   <AuthProvider>
-    <App />
+    <TextSelect>
+
+      <App />
+    </TextSelect>
   </AuthProvider>
   // </React.StrictMode>,
 )
