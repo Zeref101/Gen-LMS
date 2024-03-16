e658210131f8e429c588a1a6c650dfd29b4cb129
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 21:29:21 +0530

    Refactor Note component UI

--- src/_root/pages/Note.tsx
+++ src/_root/pages/Note.tsx
@@ -3,12 +3,7 @@ import { fetchUserNotes } from '@/lib/backend/User';
 import { DocumentData } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 
-
-
 const Note = () => {
-    // const user = localStorage.getItem("user");
-
-
     const userId = "default_userid";
     const [notes, setNotes] = useState<DocumentData[]>([]);
     const [noteKeys, setNoteKeys] = useState<string[][]>([]);
@@ -38,23 +33,38 @@ const Note = () => {
             });
         });
 
-        setNoteKeys([Array.from(newKeys)]); // Simplified to a single, flat array
+        setNoteKeys([Array.from(newKeys)]);
     }, [notes]);
 
     return (
-        <div className=' flex flex-col gap-8 justify-start'>
-            <h1>Notes</h1>
-            <div className=' flex flex-wrap gap-4 justify-center items-center'>
-                {/* Ensure noteKeys is not undefined and has length before mapping */}
-                {noteKeys && noteKeys.length > 0 && noteKeys[0].map((note, index) => (
-                    <Link to={`/notes/${note}`} key={index}>
-                        <div className=' w-40 h-40 rounded-lg bg-slate-300 hover:bg-slate-400 flex justify-center items-center'>
-                            <p>{note}</p>
-                        </div>
-                    </Link>
-                ))}
+        <>
+            <div className=' flex h-screen flex-col gap-8 justify-start items-center w-full p-4 bg-white'>
+                <h1 className='font-bold text-purple-500'>Student Notes</h1>
+                <div className=' flex flex-wrap gap-4 w-[800px] justify-start items-start'>
+                    {noteKeys && noteKeys.length > 0 && noteKeys[0].map((note, index) => (
+                        <Link to={`/notes/${note}`} key={index}>
+                            <div className="card drop-shadow-lg">
+                                <div className="tools">
+                                    <div className="circle">
+                                        <span className="red box"></span>
+                                    </div>
+                                    <div className="circle">
+                                        <span className="yellow box"></span>
+                                    </div>
+                                    <div className="circle">
+                                        <span className="green box"></span>
+                                    </div>
+                                </div>
+                                <div className=" text-white font-bold flex justify-center items-center text-[28px]">
+                                    {note}
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
+
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 export default Note;
