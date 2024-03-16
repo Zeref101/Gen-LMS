40577ca2e603dc889c95cfdf6c93f58877e156be
Shreyas <shreyas.mohanty03@gmail.com>
2024-03-16 10:22:04 +0530

    Refactor main.tsx to remove React.StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css'
 import { AuthProvider } from './context/AuthContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+  // </React.StrictMode>,
 )
